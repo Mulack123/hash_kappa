13f7b0b070af0379dc9868e83c3fe0976d177415
Mulack123 <166628249+Mulack123@users.noreply.github.com>
2024-06-05 21:54:46 +0100

    Update HashKappa.py

--- HashKappa.py
+++ HashKappa.py
@@ -28,9 +28,16 @@ def openPasswordFile(passwordFile):
     #return the list of passwords to hash and compare
     return passwords
 
-def md5_hash_string(input_string):
+def md5_hash_string(input_string, format=False):
     # Create an MD5 hash object
-    md5_hash = hashlib.md5()
+    if format == 'md5':
+        md5_hash = hashlib.md5()
+    elif format == 'NT':
+        md5_hash = hashlib.NT()
+    elif format == False:
+        md5_hash = hashlib.md5()
+    else:
+        return 'Error'
     
     # Update the hash object with the bytes of the input string
     md5_hash.update(input_string.encode('utf-8'))
@@ -41,41 +48,48 @@ def md5_hash_string(input_string):
     return hex_digest
 
 #Compare hashes to passwords in a dump
-def comparePasswordList(passwordList, hashList):
+def comparePasswordList(passwordList, hashList, format=False):
     crackedPasswords = []
     #Looping through hash list
     for i in hashList:
         #Compare md5 hash of password list to current hash
         for n in passwordList:
             #If they match, add to crackedPasswords list
-            hex_digest = md5_hash_string(n)
+            hex_digest = md5_hash_string(n, format)
             if hex_digest == i:
                 crackedPasswords.append("Hash: {} is {}".format(i, n))
+            elif hex_digest == 'Error':
+                return 'This format is not recognised'
     #Return the cracked passwords
     return crackedPasswords
 
 #Main file loop
 def mainLoop():
     # create parser
-    descStr = "This script compares an MD5 hashdump against a pre-exisiting password list"
+    descStr = "This script attempts to crack a given hash dump using a given password list"
     parser = argparse.ArgumentParser(description=descStr)
     # add expected arguments
-    parser.add_argument('--hash-dump', dest='hashFile', required=True)
-    parser.add_argument('--password-file', dest='passwordFile', required=True)
-    
+    parser.add_argument('--hash-dump', help='Required hash dump to crack', dest='hashFile', required=True)
+    parser.add_argument('--password-file', help='Password file to compare against the hash dump', dest='passwordFile', required=True)
+    parser.add_argument('--format', help='Hash algorithm', dest='hashFormat', required=False)
     args = parser.parse_args()
+
     #Passing arguments into vars
     hashFile = args.hashFile
     passwordFile = args.passwordFile
+    hashFormat = args.hashFormat
 
     hashList = openHashFile(hashFile)
     passwordList = openPasswordFile(passwordFile)
 
-    crackedPasswords = comparePasswordList(passwordList, hashList)
-    """ print(passwordList) #Testing passwords list has been correctly added
-    print(hashList) #Testing hash list has been correctly added """
-    for i in crackedPasswords:
-        print(i)
+    
+    crackedPasswords = comparePasswordList(passwordList, hashList, hashFormat)
+   
+    if crackedPasswords == 'This format is not recognised':
+        print(crackedPasswords)
+    else:
+        for i in crackedPasswords:
+            print(i)
 
 
 if __name__ == "__main__":
